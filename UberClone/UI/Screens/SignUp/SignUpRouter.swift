26daae6598c10df65dac6e4cb42ae5f516aa18ec
Gilberto Silva <betosilva.luz@gmail.com>
2023-04-30 18:53:36 -0300

    Navegando para a tela principal ao criar uma conta.

---
+++ UberClone/UI/Screens/SignUp/SignUpRouter.swift
@@ -0,0 +1,27 @@
+//
+//  SignUpRouter.swift
+//  UberClone
+//
+//  Created by Gilberto Silva on 30/04/23.
+//
+
+import Foundation
+
+public final class SignUpRouter {
+    
+    private let nav: NavigationController
+    private let mainFactory: () -> MainViewController
+   
+    public init(
+        nav: NavigationController,
+        mainFactory: @escaping () -> MainViewController
+    ) {
+        self.nav = nav
+        self.mainFactory = mainFactory
+    }
+   
+    public func goToMain() {
+        nav.pushViewController(mainFactory())
+    }
+    
+}
