1c3308e995d54a8b198dd1a4cd5bb92911c8f655
Gilberto Silva <betosilva.luz@gmail.com>
2023-05-01 19:11:35 -0300

    Adicionando mapa a tela de passageiro.

--- UberClone/UI/Screens/Passenger/PassengerViewController.swift
+++ UberClone/UI/Screens/Passenger/PassengerViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 public final class PassengerViewController: UIViewController {
     
@@ -18,18 +19,48 @@ public final class PassengerViewController: UIViewController {
         )
     }()
     
+    private let mapView: MKMapView = {
+        let view  = MKMapView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     public var logout: (() -> Void)?
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Uber"
-        self.view.backgroundColor = .white
-        self.navigationItem.setHidesBackButton(true, animated: false)
-        self.navigationItem.leftBarButtonItem = self.logoutButtonItem
+        setupView()
     }
     
     @objc private func logoutButtonTapped() {
         self.logout?()
     }
     
+}
+
+// MARK: - ViewCode
+extension PassengerViewController: ViewCode {
+    
+    func setupViewHierarchy() {
+        self.view.addSubview(mapView)
+    }
+    
+    func setupConstraints() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        
+        // mapView
+        NSLayoutConstraint.activate([
+            self.mapView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            self.mapView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            self.mapView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            self.mapView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+    
+    func setupAdditionalConfiguration() {
+        self.title = "Uber"
+        self.view.backgroundColor = .white
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        self.navigationItem.leftBarButtonItem = self.logoutButtonItem
+    }
 }
