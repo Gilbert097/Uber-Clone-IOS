d4467e4885bf4665c510a09c40030df39a79de10
Gilberto Silva <betosilva.luz@gmail.com>
2023-05-06 22:05:00 -0300

    Recuperando informações do usuário.

---
+++ UberClone/Domain/UseCases/Autentication/GetCurrentUser.swift
@@ -0,0 +1,12 @@
+//
+//  GetCurrentUser.swift
+//  UberClone
+//
+//  Created by Gilberto Silva on 06/05/23.
+//
+
+import Foundation
+
+public protocol GetCurrentUser {
+    func get() -> UserModel?
+}
--- UberClone/Presentation/Passenger/PassengerPresenter.swift
+++ UberClone/Presentation/Passenger/PassengerPresenter.swift
@@ -12,13 +12,19 @@ public final class PassengerPresenter {
     private let loadingView: LoadingView
     private let callRace: CallRace
     private let logoutAuth: LogoutAuth
+    private let getCurrentUser: GetCurrentUser
     var dismiss: (() -> Void)!
     
-    public init(alertView: AlertView, loadingView: LoadingView, callRace: CallRace, logoutAuth: LogoutAuth) {
+    public init(alertView: AlertView,
+                loadingView: LoadingView,
+                callRace: CallRace,
+                logoutAuth: LogoutAuth,
+                getCurrentUser: GetCurrentUser) {
         self.alertView = alertView
         self.loadingView = loadingView
         self.callRace = callRace
         self.logoutAuth = logoutAuth
+        self.getCurrentUser = getCurrentUser
     }
     
     public func logout() {
@@ -30,9 +36,10 @@ public final class PassengerPresenter {
     }
     
     public func callRaceAction(request: CallRaceRequest) {
+        guard let currentUser = self.getCurrentUser.get() else { return }
         self.loadingView.display(viewModel: .init(isLoading: true))
-        let model = CallRaceModel(email: "Gilberto.silva2@gmail.com",
-                                  name: "Gilberto2",
+        let model = CallRaceModel(email: currentUser.email,
+                                  name: currentUser.name,
                                   latitude: String(request.latitude),
                                   longitude: String(request.longitude))
         self.callRace.request(model: model) { [weak self] result in
